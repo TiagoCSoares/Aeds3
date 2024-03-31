f924c431e5d2ea636b85ab7090c36d98438b68d1
TiagoCSoares <tiagocsoares22@gmail.com>
2024-03-30 21:44:43 -0300

    feat Inicializando o diretório Output

--- Trabalho2/main.c
+++ Trabalho2/main.c
@@ -6,48 +6,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <dirent.h>
 
-int main() {
-    char *nomeArquivo = "Labirintos/labirinto"; // Parte fixa do nome do arquivo
-    char *extensao = ".txt"; // Extensão do arquivo
-    No* nos[LINHAS][COLUNAS];
-    
-    // Alocar memória para o labirinto fora do loop
-    char labirinto[LINHAS][COLUNAS];
-    
+
+void lerLabirintoEConstruirGrafo(char *nomeArquivo, char labirinto[LINHAS][COLUNAS], No* nos[LINHAS][COLUNAS]) {
+    lerLabirinto(nomeArquivo, labirinto);
+    criarGrafo(labirinto, nos);
+}
+
+void liberarNos(No* nos[LINHAS][COLUNAS]) {
     for(int i = 0; i < LINHAS; i++) {
         for(int j = 0; j < COLUNAS; j++) {
-            //nos[i][j] = NULL;
-
-            nos[i][j] = (No*)malloc(sizeof(No)); // Alocar memória para o nó
-            nos[i][j]->percorrido = false; // Inicializar membros percorrido e visitado
-            nos[i][j]->visitado = false;
+            free(nos[i][j]);
         }
     }
+}
 
+char *construirNomeArquivoCompleto(char *nomeArquivo, char *extensao, int numero) {
+    char numeroStr[10];
+    sprintf(numeroStr, "%d", numero);
+    size_t tamanho = strlen(nomeArquivo) + strlen(extensao) + strlen(numeroStr) + 1;
+    char *nomeCompleto = (char *)malloc(tamanho);
+    sprintf(nomeCompleto, "%s%s%s", nomeArquivo, numeroStr, extensao);
+    return nomeCompleto;
+}
+
+void buscarEmProfundidade(char labirinto[LINHAS][COLUNAS], No* nos[LINHAS][COLUNAS], char *extensao, char *nomeArquivo) {
     limparArquivo("Output/ProfundidadeOutput.txt");
-
-    // Navegar pelos cinco labirintos
     for(int i = 1; i <= 5; i++) {
-        // Converter o número inteiro para uma string
-        char numero[10];
-        sprintf(numero, "%d", i);
-
-        // Determinar o tamanho necessário para o nome do arquivo completo
-        size_t tamanhoNomeArquivoCompleto = strlen(nomeArquivo) + strlen(numero) + strlen(extensao) + 1;
-        // Alocar memória para o nome do arquivo completo
-        char *nomeArquivoCompleto = (char *)malloc(tamanhoNomeArquivoCompleto);
-
-        // Construir o nome do arquivo completo
-        strcpy(nomeArquivoCompleto, nomeArquivo);
-        strcat(nomeArquivoCompleto, numero);
-        strcat(nomeArquivoCompleto, extensao);
-
-        // Ler o labirinto do arquivo
-        lerLabirinto(nomeArquivoCompleto, labirinto);
-
-        // Criar o grafo a partir do labirinto
-        criarGrafo(labirinto, nos);
+        char *nomeArquivoCompleto = construirNomeArquivoCompleto(nomeArquivo, extensao, i);
+        lerLabirintoEConstruirGrafo(nomeArquivoCompleto, labirinto, nos);
 
         No *entrada = NULL;
         No *saida = NULL;
@@ -61,38 +50,15 @@ int main() {
         buscaPorProfundidade(nos, entrada, saida, &pilha);
         escreverOutput("Output/ProfundidadeOutput.txt", pilha);
 
-        // Liberar a memória alocada dinamicamente
         free(nomeArquivoCompleto);
     }
+}
 
-
-
-
-
-   limparArquivo("Output/LarguraOutput.txt");
-
-    // Navegar pelos cinco labirintos
+void buscarEmLargura(char labirinto[LINHAS][COLUNAS], No* nos[LINHAS][COLUNAS], char *extensao, char *nomeArquivo) {
+    limparArquivo("Output/LarguraOutput.txt");
     for(int i = 1; i <= 5; i++) {
-
-        
-        char numero[10]; 
-        sprintf(numero, "%d", i);
-
-        // Determinar o tamanho necessário para o nome do arquivo completo
-        size_t tamanhoNomeArquivoCompleto = strlen(nomeArquivo) + strlen(numero) + strlen(extensao) + 1;
-        // Alocar memória para o nome do arquivo completo
-        char *nomeArquivoCompleto = (char *)malloc(tamanhoNomeArquivoCompleto);
-
-        // Construir o nome do arquivo completo
-        strcpy(nomeArquivoCompleto, nomeArquivo);
-        strcat(nomeArquivoCompleto, numero);
-        strcat(nomeArquivoCompleto, extensao);
-
-        // Ler o labirinto do arquivo
-        lerLabirinto(nomeArquivoCompleto, labirinto);
-
-        // Criar o grafo a partir do labirinto
-        criarGrafo(labirinto, nos);
+        char *nomeArquivoCompleto = construirNomeArquivoCompleto(nomeArquivo, extensao, i);
+        lerLabirintoEConstruirGrafo(nomeArquivoCompleto, labirinto, nos);
 
         No *entrada = NULL;
         No *saida = NULL;
@@ -104,15 +70,48 @@ int main() {
         entrada->visitado = true;
 
         No* paiSaida = buscaPorLargura(nos, *entrada, *saida, &fila, labirinto);
-        if(paiSaida == NULL) {
-            printf("NULL\n");
-        }
-
         escreverOutputFila("Output/LarguraOutput.txt", &fila, paiSaida, *saida, *entrada);
 
-        // Liberar a memória alocada dinamicamente
         free(nomeArquivoCompleto);
     }
+}
+
+void criarPastaSeNaoExistir(const char *nomePasta) {
+    struct dirent *dir;
+    DIR *diretorio = opendir(nomePasta);
+    
+    if (!diretorio) {
+        mkdir(nomePasta, 0700); // Permissões: 0700 (permissões rwx para o proprietário)
+    } else {
+        closedir(diretorio);
+    }
+}
+
+
+
+
+int main() {
+    char *nomeArquivo = "Labirintos/labirinto";
+    char *extensao = ".txt";
+    No* nos[LINHAS][COLUNAS];
+    char labirinto[LINHAS][COLUNAS];
+
+
+    //const char *nomePasta = "Output"; // Nome da pasta a ser criada
+    criarPastaSeNaoExistir("Output");
+       
+    for(int i = 0; i < LINHAS; i++) {
+        for(int j = 0; j < COLUNAS; j++) {
+            nos[i][j] = (No*)malloc(sizeof(No));
+            nos[i][j]->percorrido = false;
+            nos[i][j]->visitado = false;
+        }
+    }
+
+    buscarEmProfundidade(labirinto, nos, extensao, nomeArquivo);
+    buscarEmLargura(labirinto, nos, extensao, nomeArquivo);
+
+    liberarNos(nos);
 
     return 0;
 }
